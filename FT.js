de717b1280973294e946120f46619993932a81e7
Ninoo16 <133487819+Ninoo16@users.noreply.github.com>
2023-07-06 19:00:48 +0200

    now i created two functions that make us play against computer 'rock, paper, scissors' and made them return one of those randomly and wrote a string whcih determine the winner

--- FT.js
+++ FT.js
@@ -1,1 +1,24 @@
-console.log("hello world");
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() *3);
+    switch (randomNumber) {
+        case 0:
+            return "rock";
+            case 1:
+                return "paper";
+                case 3:
+                    return "scissors";
+    }
+}
+console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection === "paper" && computerSelection === "rock") {
+    return "You Lose! Paper beats Rock" ;
+    } else if(playerSelection === "scissors" && computerSelection === "paper") {
+        return "you lose! Scissors beats paper" ;
+    } else if(playerSelection === "rock" && computerSelection === "scissors") {
+        return "you lose! Scissors beat paper" ;
+    } else {
+        return " you win! congrats";
+    }
+}
+
