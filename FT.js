1ce1c441b7ff58e90ad637e04733a2d071475ba5
Ninoo16 <133487819+Ninoo16@users.noreply.github.com>
2023-07-07 12:20:55 +0200

    now we used the 'while' loop to make the prompt repeatedly appear again after the input of user to play a 5round game not one round only based on the given booler which is (round <= 5) in this case

--- FT.js
+++ FT.js
@@ -11,14 +11,37 @@ function getComputerChoice() {
 }
 console.log(getComputerChoice());
 function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    
+
     if(playerSelection === "paper" && computerSelection === "rock") {
-    return "You Lose! Paper beats Rock" ;
+    return "You win! Paper beats Rock" ;
     } else if(playerSelection === "scissors" && computerSelection === "paper") {
-        return "you lose! Scissors beats paper" ;
+        return "you win! Scissors beats paper" ;
     } else if(playerSelection === "rock" && computerSelection === "scissors") {
-        return "you lose! Scissors beat paper" ;
+        return "you win! rock beat scissors" ;
     } else {
-        return " you win! congrats";
+        return " you lose! try again";
+    }
+
+}
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let round = 1;
+    while (round <= 5) {
+        const playerSelection = prompt("enter your choice: rock, paper, scissors");
+        const computerSelection = ["rock", "paper","scissors"]
+        [Math.floor(Math.random()*3)];
+        const result = playRound(playerSelection, computerSelection);
+        console.log(result);
+        if (result.includes("win")) {
+            playerScore++;
+        } else if (result.includes("lose")) {
+            computerScore++
+        }
+        console.log(`player score: ${playerScore} computer score ${computerScore}`);
+        round++;
     }
 }
-
+    
